3258e81900dcf46c61ffadbc5b28f3386df31b19
Lucas <lucas.ce.bertn@gmail.com>
2020-11-11 00:32:29 -0300

    feat(acesso): limita acesso para execução de server. resolves #2

---
+++ nivel2/src/index.ts
@@ -0,0 +1,120 @@
+import Docker, {Container} from 'dockerode'
+import ssh2 from 'ssh2'
+import NodeRSA from 'node-rsa'
+
+const docker = new Docker()
+
+const key = new NodeRSA({b: 1024})
+const privKey = key.exportKey('pkcs1-private-pem')
+
+const TIMEOUT = 100000
+
+new ssh2.Server({
+  hostKeys: [ privKey ],
+  banner: "Welcome!",
+  ident: "ssh-sandboxes",
+}, (client) => {
+  console.log("Client connected!")
+    
+  client.on('authentication', (ctx) => {
+    // Blindly accept all connections. Only one per IP address allowed, though.
+    ctx.accept()        
+  }).on('ready', () => {
+    console.log('Client authenticated!')
+    client.on('session', function(accept) {
+      console.log('Client wants new session')
+      var session = accept()
+      session.once('pty', (accept) => {
+        accept()
+      })
+      session.once('shell', (accept) => {
+        console.log('Client wants a shell!')
+        let container: Container | undefined
+
+        // Accept the connection and get a bidirectional stream.
+        const stream = accept()
+
+        var cleanupStream = function() {
+          if (stream.timeoutId) {
+            clearTimeout(stream.timeoutId)
+          }
+
+          if (container) {
+            container.remove({force: true}, function(err) {
+              if (err && container) {
+                console.log('Error removing container %s: %s', container.id, err)
+              }
+              console.log('Removed container')
+            })
+          }
+        }
+
+        docker.createContainer({
+          Cmd: [ '/bin/bash' ],
+          Image: 'ubuntu:latest',
+          OpenStdin: true,
+          Tty: true
+          }, function (err, newContainer) {
+          if (err) {
+            console.log(err)
+            return
+          }
+
+          container = newContainer
+
+          if(!container)
+            return 
+
+          container.attach({
+            stream: true, stdin: true, stdout: true, stderr: true
+          }, function (_, ttyStream) {
+            if(!newContainer || !ttyStream)
+              return
+
+            console.log("Attached to container " + newContainer.id)
+
+            ttyStream.pipe(stream);
+
+            // Attach client stream to stdin of container
+            stream.pipe(ttyStream);
+
+            // Start the container
+            newContainer.start((err) => {
+              if (err) {
+                console.error('Unable to start container', err)
+                return
+              }
+              console.log("Container started!")
+            })
+          })
+        })
+
+        const onTimeout = function () {
+          console.log('Closing session due to timeout')
+          stream.close()
+        }
+
+        stream.on('data', function() {
+          // Reset timeout
+          if (stream.timeoutId) {
+            clearTimeout(stream.timeoutId)
+          }
+          stream.timeoutId = setTimeout(onTimeout, TIMEOUT)
+        })
+
+        stream.on('end', () => {
+          console.log('Stream disconnected!')
+          cleanupStream()
+        })
+      })
+    })
+  }).on('abort', () => {
+    console.log('Client aborted!')
+  }).on('end', () => {
+    console.log('Client disconnected!')
+  })
+
+}).listen(2222, '127.0.0.1', () => {
+  console.log('Listening on port 2222')
+})
+
