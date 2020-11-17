a0104157e3b76c2240744fe499078f06a2d1e559
Lucas <lucas.ce.bertn@gmail.com>
2020-11-17 09:25:14 -0300

    chore: adiciona recomendações de valores para variaveis do terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
+  description = "The prefix which should be used for all resources in this example (ex: ssh-game)"
 }
 
 variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
+  description = "The Azure Region in which all resources in this example should be created. (ex: brazilsouth)"
 }
